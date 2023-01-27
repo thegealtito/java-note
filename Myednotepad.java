2b7853bcf4c8ced578995222398b33b84a381006
thegealtito <abigealbolatito@gmail.com>
2023-01-27 13:52:05 +0100

    added cut

--- Myednotepad.java
+++ Myednotepad.java
@@ -15,7 +15,7 @@ public class Myednotepad {
         Edit = new JMenu("Edit");
         Format = new JMenu("Format");
 
-        JMenuItem save, i2, i3, i4, i5, ia, paste, copy, i8, i9, ib;
+        JMenuItem save, i2, i3, i4, i5, ia, paste, copy, i8, i9, cut;
 
         copy = new JMenuItem(new AbstractAction("Copy") {
             // when copy menu is click perform the action in the following method
@@ -25,10 +25,19 @@ public class Myednotepad {
             }
         });
         paste = new JMenuItem(new AbstractAction("Paste") {
-            // when copy menu is click perform the action in the following method
+            // when paste menu is click perform the action in the following method
             public void actionPerformed(ActionEvent e) {
                 // paste text in the message area
                 messageArea.paste();
+            }
+        });
+
+        cut = new JMenuItem(new AbstractAction("cut") {
+            // when cut menu is click perform the action in the following method
+            public void actionPerformed(ActionEvent e) {
+                // cut text in the message area
+                messageArea.cut();
+                System.out.println("go and work");
             }
         });
 
@@ -40,7 +49,7 @@ public class Myednotepad {
         i5 = new JMenuItem("Exit");
         i8 = new JMenuItem("delete");
         i9 = new JMenuItem("word wrap");
-        ib = new JMenuItem("cut");
+        cut = new JMenuItem("cut");
 
         messageArea.setBounds(0, 0, 400, 400);
         FileMenu.add(save);
@@ -53,7 +62,7 @@ public class Myednotepad {
         Edit.add(paste);
         Format.add(i8);
         Format.add(i9);
-        Format.add(ib);
+        Format.add(cut);
         Edit.add(Format);
 
         f.add(messageArea);
